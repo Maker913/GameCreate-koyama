b8bf342bacc0da831dbbb41adf0447c64b5af7dd
koyama <youtub8756@gmail.com>
2019-07-06 16:00:22 +0900

    Revert "CPU 置けない"
    This reverts commit e4172d317e39ccc4f42f114ff865c59e8ebf9e3a.

--- Tic-tac-toe_project/Assets/Script/Buttons.cs
+++ Tic-tac-toe_project/Assets/Script/Buttons.cs
@@ -8,25 +8,14 @@ public class Buttons : MonoBehaviour
     public Button button;
     public Text buttonText;
     private Board board;
-    private CPU CPU;
     public void SetGameControllerReference(Board boards)
     {
         board = boards;
-    }
-    public void SetGameControllerReferences(CPU cpu)
-    {
-        CPU = cpu;
     }
     public void SetSpace()
     {
         buttonText.text = board.GetPlayerSide();
         button.interactable = false;
         board.TurnEnd();
-    }
-    public void SetSpaceOne()
-    {
-        buttonText.text = CPU.GetPlayerSide();
-        button.interactable = false;
-        CPU.TurnEnd();
     }
 }
